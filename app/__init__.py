f9ce4a948e06727538141521b5274101311c491f
Dusan <dusanrangelov@yahoo.com>
2026-04-28 10:00:21 +0200

    Formated index.html

--- app/__init__.py
+++ app/__init__.py
@@ -10,11 +10,12 @@ def create_app(config_class=Config):
     # app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
     app.config.from_object(config_class)
     webview.create_window('Workout Interval Timer', 'app/templates/index.html', width=800, height=800)
-    webview.start()
+
 
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
+    webview.start()
 
     return app
 
