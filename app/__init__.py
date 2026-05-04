ad6f9da0e97db51f7a3655c913ab51ad9eabae2e
Dusan <dusanrangelov@yahoo.com>
2026-05-04 11:47:58 +0200

    Changed webview logic to be presented in a separate window

--- app/__init__.py
+++ app/__init__.py
@@ -9,12 +9,13 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     # app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
     app.config.from_object(config_class)
-    webview.create_window('Workout Interval Timer', 'app/templates/index.html', width=800, height=800)
+    webview.create_window('Workout Interval Timer', app, width=800, height=800)
 
 
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
+
     webview.start()
 
     return app
